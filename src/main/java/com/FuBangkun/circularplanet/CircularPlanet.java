bbe983dfc39be41204c16fda3c93690237dc9bbd
FuBangkun <mc14705506502@163.com>
2024-06-28 15:12:03 +0800

    1.0.1

---
+++ src/main/java/com/FuBangkun/circularplanet/CircularPlanet.java
@@ -0,0 +1,27 @@
+package com.FuBangkun.circularplanet;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+    modid = "circularplanet",
+    name = "Circular Planet",
+    version = "1.0.1",
+    acceptedMinecraftVersions = "[1.12.2]",
+    dependencies =
+            "required-after:galacticraftcore;" +
+                    "after:galacticraftplanets;" +
+                    "after:galaxyspace;" +
+                    "after:exoplanets;" +
+                    "after:moreplanets;" +
+                    "after:asmodeuscore;" +
+                    "after:starmaker"
+)
+public class CircularPlanet {
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        Logger logger = event.getModLog();
+    }
+}
